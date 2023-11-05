173e1d91c81801ef3669dc88347432b2a5539287
aniljos <anil.jos@gmail.com>
2023-11-05 23:06:44 +0530

    Day 1

---
+++ CSharpApplications/SampleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using SampleLib;
+
+
+namespace SampleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 100;
+            Console.WriteLine("Hello, World!");
+            //DataTypes();
+            CreateStudents();
+            Console.WriteLine("App is over");
+        }
+
+        static void DataTypes()
+        {
+            int x = 10;
+            Console.WriteLine("x: " + x);
+
+            User user = new User();
+            user.Name = "John";
+            user.Age = 19;
+            Console.WriteLine("User Name: {0}, Age: {1}", user.Name, user.Age);
+
+        }
+
+        static void CreateStudents()
+        {
+            Student student = new Student();
+
+            //Console.WriteLine($" Id: {student.Id}, Name: {student.Name}, Age: {student.Age}");
+            student.Display();
+            student.Id = 100;
+            student.Name = "Alice";
+            student.Age = 20;
+
+            //Console.WriteLine($" Id: {student.Id}, Name: {student.Name}, Age: {student.Age}");
+            student.Display();
+
+            Student student1 = new Student(101, "John", 19);
+            //Console.WriteLine($" Id: {student1.Id}, Name: {student1.Name}, Age: {student1.Age}");
+            student.Display();
+
+        }
+    }
+}
---
+++ CSharpApplications/SampleLib/Student.cs
@@ -0,0 +1,70 @@
+﻿namespace SampleLib
+{
+    public class Student
+    {
+        private int _id;
+        private string _name;
+        private int _age;
+
+
+        //Construtors
+        // new Student()
+        public Student()
+        {
+            Console.WriteLine("Involing the no args constructor");
+        }
+
+        // new Student(100, "Alice", 20)
+        public Student(int id, string name, int age)
+        {
+            Console.WriteLine("Invoking the constructor with args");
+            this._id = id;
+            this.Name = name;
+            this._age = age;
+        }
+
+
+        // Student s = new Student();
+        // s.Id = 100; // invokes the set block
+        // int id = s.Id // invokes the get block
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        public string Name { get { return _name; }  set { _name = value; } }
+
+        public int Age { 
+            get { return _age;  } 
+            set {
+
+                if(value > 0)
+                {
+                    _age = value;
+                }
+                
+            } 
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($" Id: {this.Id}, Name: {this.Name}, Age: {Age}");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
